65dcfb77378f4e12c2b55269e391cc32b1a4613b
jairoahc <jairohernandez010399@gmail.com>
2022-10-26 11:15:05 -0500

    Se separaron el html y el script en otro repositorio, en este repositorio solo permanecera el back de la creacion del api

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 //Importar libreria express
 const express = require ("express");
+//Importar libreria cors (celador de las peticiones al servidor)
+const cors = require ("cors")
 //Llamar una constante para ejecutar las funciones de express de manera mas fácil:
 const app = express();
 //Definir el puerto que se va a escuchar en una constante:
 const port = 5000;
 
 //Especificar las rutas y que funciones ejecutaran esas rutas, por ejemplo, get:
+app.use(cors())
 
 /* Por medio de un input el estudiante debe desarrollar una pagina web que consuma un api De su autoria con lo siguiente
 En la pagina se debe recibir el nombre de una persona, pasarse al api y esta debe responder con un saludo {message: "Hola <el nombre> como estas?"}
@@ -16,24 +19,6 @@ En la pagina se debe recibir el nombre de una persona, pasarse al api y esta deb
  
     return res.status(200).json({message:`Hola ${req.query.name}, ¿Como estas?`})
 })
- 
-
-
- function getapi(){
-
-    let name = document.getElementById("name").value
-
-    fetch (`http://127.0.0.1:5000/apiJairo?name=${name}`)
-    .then((res) => {
-        return res.json();
-    })
-    .then((data) => {
-        console.log(data)
-    })
-    
- } 
-
-
 
 //Establecer a través de que puerto estará procesando o escuchando el servidor
 app.listen(port,() => console.log(`Servidor escuchando el puerto ${port}` ))
